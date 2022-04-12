900412b5f12e4c560b237c78df5a43a2dcb7b3f8
Sourav_Bhattacharya10 <sourav.bhattacharya3@gmail.com>
2022-04-12 20:23:20 +0530

    CleaningOld

--- src/ui/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import {getImagesReducer} from './containers/reducers';
-
-const rootReducer = combineReducers({
-    images: getImagesReducer
-})
-
-export default rootReducer;
